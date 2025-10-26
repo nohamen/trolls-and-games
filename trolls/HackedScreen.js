a7988baf33d4ba287313f492a0a87e43075ff0c3
hamen <noeln4363@gmail.com>
2025-10-26 17:05:34 -0400

    Update HackedScreen.js

--- trolls/HackedScreen.js
+++ trolls/HackedScreen.js
@@ -1,1 +1,1 @@
-alert("by nohamen, This Can Damage Your Computer Or You'll have to restart It PLEASE NOTE THAT I CAN'T GET SUED FOR THIS USE AT RISK.");
+alert("by nohamen, This Can Damage Your Computer Or You'll have to restart It PLEASE NOTE THAT I CAN'T GET SUED FOR THIS USE AT OWN RISK.");
