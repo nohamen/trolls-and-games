4459caac57af79b0215e0149980db3dda64e43c1
hamen <noeln4363@gmail.com>
2025-10-26 17:10:20 -0400

    Create Cookie background.js

---
+++ trolls/Cookie background.js
@@ -0,0 +1,3 @@
+javascript: function applyBackgroundImage(element) { 
+
+ childNodes[i].style.backgroundImage = "url(<img src="https://media.istockphoto.com/id/1285382015/vector/tasty-thin-and-crispy-chocolate-chip-cookies-vector-pattern.jpg?s=612x612&amp;w=0&amp;k=20&amp;c=_zebZS_gjaEOaS73jvlGFfIE3Y5lhi1QskJMNiFxBoE=" alt="55,300+ Cookie Background Stock Illustrations, Royalty-Free ..."/>)";)";
